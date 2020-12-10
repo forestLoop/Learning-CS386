e956dbdfeaa5d69e3614ca9056c3654484eb271a
keithnull <keith1126@126.com>
2020-12-10 20:34:57 +0800

    Implement naive model and dataset

---
+++ qa4camera/dataset.py
@@ -0,0 +1,68 @@
+import os
+import csv
+import cv2
+import numpy as np
+
+from torch.utils.data import Dataset, DataLoader
+
+
+class ImageDataset(Dataset):
+
+    def __init__(self, dataset_root, scenes):
+        self.images = dict()
+        self.data = []
+        print("Loading dataset...")
+        sort_result_dir = os.path.join(
+            dataset_root, "score_and_sort", "Training", "sort")
+        scenes_dir = os.path.join(dataset_root, "Training")
+        for scene_id in scenes:
+            print(f"Scene {scene_id:03d}")
+            # rankings[image_name] = (Color, Exposure, Noise, Texture)
+            rankings = dict()
+            with open(os.path.join(sort_result_dir, "{:03d}.csv".format(scene_id))) as f:
+                reader = csv.reader(f)
+                next(reader)  # skip header row
+                for row in reader:
+                    rankings[row[0]] = list(map(float, row[1:]))
+            image_folder = os.path.join(scenes_dir, "{:03d}".format(scene_id))
+            for image_name in rankings.keys():
+                image_path = os.path.join(image_folder, image_name)
+                image = cv2.imread(image_path)
+                # (height, weight, channel) -> (channel, weight, height)
+                image = np.transpose(image, (2, 0, 1))
+                image = cv2.resize(image, (3024, 4032))
+                self.images[image_name] = image
+                r1 = np.array(rankings[image_name])
+                for another_image_name in rankings.keys():
+                    if image_name == another_image_name:
+                        continue
+                    r2 = np.array(rankings[another_image_name])
+                    self.data.append(
+                        (image_name, another_image_name, (r1 < r2) * 1)
+                    )
+        print(
+            f"Dataset loadeded: {len(self.images)} images and {len(self.data)} pairs.")
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        item = self.data[idx]
+        image1 = self.images[item[0]]
+        image2 = self.images[item[1]]
+        image = np.dstack((image1, image2))
+        assert image.shape[2] == 6
+        labels = item[2]
+        return image, labels
+
+
+if __name__ == "__main__":
+    print("Note: this file is not expected to be run directly!")
+    print("Test ImageDataset:")
+    d = ImageDataset("C:\\MyCode\\DIP\\DIP1\\dataset", tuple(range(1, 10)))
+    print(len(d))
+    for i in range(100):
+        x = d[i]
+        print(x[0].shape, x[1])
---
+++ qa4camera/model.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+import torchvision
+import numpy as np
+
+
+class MyModel(nn.Module):
+    def __init__(self):
+        super(MyModel, self).__init__()
+        self.pretrained_resnet = torchvision.models.resnet101(pretrained=True)
+        # set input channels to 6 as we stack up two images
+        self.pretrained_resnet.conv1 = nn.Conv2d(6, 64, kernel_size=(
+            7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        # set output dimension to 4
+        self.pretrained_resnet.fc = nn.Linear(2048, 4)
+
+    def forward(self, x):
+        x = self.pretrained_resnet(x)
+        # add sigmoid function before output
+        x = torch.sigmoid(x)
+        return x
+
+
+if __name__ == "__main__":
+    import time
+    print("Note: this file is not expected to be run directly!")
+    print("Test MyModel:")
+    model = MyModel()
+    for shape in ((10, 6, 300, 400), (1, 6, 3000, 4000)):
+        tik = time.time()
+        sample_input = np.random.randint(0, 256, shape)
+        sample_input = torch.from_numpy(sample_input).float()
+        print("input:", sample_input.shape)
+        sample_output = model(sample_input)
+        print("output:", sample_output)
+        print("time: ", time.time() - tik)
