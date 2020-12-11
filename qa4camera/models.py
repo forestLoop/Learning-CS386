40eb0cc898ed4f046b394ed829a3240122686ce3
Keith Null <keith1126@126.com>
2020-12-11 22:04:26 +0800

    Fix models

--- qa4camera/models.py
+++ qa4camera/models.py
@@ -25,9 +25,9 @@ class Resnet101(nn.Module):
 class Resnet50(nn.Module):
     def __init__(self):
         super(Resnet50, self).__init__()
-        self.pretrained_resnet = torchvision.models.resnet101(pretrained=True)
+        self.pretrained_resnet = torchvision.models.resnet50(pretrained=True)
         # set input channels to 6 as we stack up two images
-        self.pretrained_resnet.conv1 = Conv2d(
+        self.pretrained_resnet.conv1 = nn.Conv2d(
             6, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False
         )
         # set output dimension to 4
@@ -40,17 +40,46 @@ class Resnet50(nn.Module):
         return x
 
 
+class Resnet50Extended(nn.Module):
+    def __init__(self):
+        super(Resnet50Extended, self).__init__()
+        self.pretrained_resnet = torchvision.models.resnet50(pretrained=True)
+        # set input channels to 6 as we stack up two images
+        self.pretrained_resnet.conv1 = nn.Conv2d(
+            6, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False
+        )
+        self.pretrained_resnet.fc = nn.Linear(2048, 2048)
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(2048, 4)
+        self.sg = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.pretrained_resnet(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        x = self.sg(x)
+        return x
+
+
 if __name__ == "__main__":
     import time
 
     print("Note: this file is not expected to be run directly!")
-    print("Test Resnet101:")
-    model = Resnet101()
-    for shape in ((10, 6, 300, 400), (1, 6, 3000, 4000)):
+    enable_cuda = True
+    for name, model in (
+        ("Resnet50", Resnet50()),
+        ("Resnet50Extended", Resnet50Extended()),
+        ("Resnet101", Resnet101()),
+    ):
+        print(f"--- {name} ---")
         tik = time.time()
-        sample_input = np.random.randint(0, 256, shape)
+        sample_input = np.random.randint(0, 256, (10, 6, 300, 400))
         sample_input = torch.from_numpy(sample_input).float()
+        if enable_cuda and torch.cuda.is_available():
+            print("Use CUDA")
+            model = model.cuda()
+            sample_input = sample_input.cuda()
         print("input:", sample_input.shape)
         sample_output = model(sample_input)
-        print("output:", sample_output)
-        print("time: ", time.time() - tik)
+        print("output:", sample_output.shape)
+        print(f"time: {time.time() - tik:.4f}s")
