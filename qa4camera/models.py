513e5cf2c8621983dd0cae1c583f04e41d9a1f5b
Keith Null <keith1126@126.com>
2020-12-11 16:12:05 +0800

    Add more resnet

--- qa4camera/models.py
+++ qa4camera/models.py
@@ -4,13 +4,32 @@ import torchvision
 import numpy as np
 
 
-class MyModel(nn.Module):
+class Resnet101(nn.Module):
     def __init__(self):
-        super(MyModel, self).__init__()
+        super(Resnet101, self).__init__()
         self.pretrained_resnet = torchvision.models.resnet101(pretrained=True)
         # set input channels to 6 as we stack up two images
-        self.pretrained_resnet.conv1 = nn.Conv2d(6, 64, kernel_size=(
-            7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        self.pretrained_resnet.conv1 = nn.Conv2d(
+            6, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False
+        )
+        # set output dimension to 4
+        self.pretrained_resnet.fc = nn.Linear(2048, 4)
+
+    def forward(self, x):
+        x = self.pretrained_resnet(x)
+        # add sigmoid function before output
+        x = torch.sigmoid(x)
+        return x
+
+
+class Resnet50(nn.Module):
+    def __init__(self):
+        super(Resnet50, self).__init__()
+        self.pretrained_resnet = torchvision.models.resnet101(pretrained=True)
+        # set input channels to 6 as we stack up two images
+        self.pretrained_resnet.conv1 = Conv2d(
+            6, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False
+        )
         # set output dimension to 4
         self.pretrained_resnet.fc = nn.Linear(2048, 4)
 
@@ -23,9 +42,10 @@ class MyModel(nn.Module):
 
 if __name__ == "__main__":
     import time
+
     print("Note: this file is not expected to be run directly!")
-    print("Test MyModel:")
-    model = MyModel()
+    print("Test Resnet101:")
+    model = Resnet101()
     for shape in ((10, 6, 300, 400), (1, 6, 3000, 4000)):
         tik = time.time()
         sample_input = np.random.randint(0, 256, shape)
