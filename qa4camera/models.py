1ae6ea61ee07bd15c6d9a032a49a4c58ace05a16
Keith Null <keith1126@126.com>
2020-12-13 16:27:55 +0800

    Add resnet18

--- qa4camera/models.py
+++ qa4camera/models.py
@@ -61,6 +61,24 @@ class Resnet50Extended(nn.Module):
         return x
 
 
+class Resnet18(nn.Module):
+    def __init__(self):
+        super(Resnet18, self).__init__()
+        self.pretrained_resnet = torchvision.models.resnet18(pretrained=True)
+        # set input channels to 6 as we stack up two images
+        self.pretrained_resnet.conv1 = nn.Conv2d(
+            6, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False
+        )
+        # set output dimension to 4
+        self.pretrained_resnet.fc = nn.Linear(512, 4)
+
+    def forward(self, x):
+        x = self.pretrained_resnet(x)
+        # add sigmoid function before output
+        x = torch.sigmoid(x)
+        return x
+
+
 if __name__ == "__main__":
     import time
 
