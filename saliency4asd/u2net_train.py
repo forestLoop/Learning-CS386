0ef723a8a6a92052840bbccd284e262f2590d116
Keith Null <keith1126@126.com>
2020-12-16 20:33:42 +0800

    Add validation in training and refactor some code

--- saliency4asd/u2net_train.py
+++ saliency4asd/u2net_train.py
@@ -1,34 +1,95 @@
 import torch
-import torchvision
 from torch.autograd import Variable
 import torch.nn as nn
-import torch.nn.functional as F
 
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
+from torch.utils.data import DataLoader
+from torchvision import transforms
 import torch.optim as optim
-import torchvision.transforms as standard_transforms
 
-import numpy as np
-import glob, os
+import glob
+import os
 
-from data_loader import Rescale
 from data_loader import RescaleT
 from data_loader import RandomCrop
-from data_loader import ToTensor
 from data_loader import ToTensorLab
 from data_loader import SalObjDataset
 
 from model import U2NET
-from model import U2NETP
 
-# ------- 1. define loss function --------
 
-bce_loss = nn.BCELoss(size_average=True)
+def validate(net, val_dataloader):
+    print("--- Validation ---")
+    net.eval()
+    batch_num = 0
+    running_loss, running_tar_loss = 0.0, 0.0
+    with torch.no_grad():
+        for _, data in enumerate(val_dataloader):
+            batch_num += 1
+
+            inputs, labels = data["image"], data["label"]
+            inputs = inputs.type(torch.FloatTensor)
+            labels = labels.type(torch.FloatTensor)
+
+            # wrap them in Variable
+            if torch.cuda.is_available():
+                inputs_v, labels_v = (
+                    Variable(inputs.cuda(), requires_grad=False),
+                    Variable(labels.cuda(), requires_grad=False),
+                )
+            else:
+                inputs_v, labels_v = Variable(inputs, requires_grad=False), Variable(
+                    labels, requires_grad=False
+                )
+            # forward
+            d0, d1, d2, d3, d4, d5, d6 = net(inputs_v)
+            loss2, loss = muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, labels_v)
+            # # print statistics
+            running_loss += loss.item()
+            running_tar_loss += loss2.item()
+            # del temporary outputs and loss
+            del d0, d1, d2, d3, d4, d5, d6, loss2, loss
+    avg_loss, avg_tar = running_loss / batch_num, running_tar_loss / batch_num
+    print(f"val loss: {avg_loss:.3f}, tar: {avg_tar:.3f}")
+    return avg_loss, avg_tar
+
+
+def load_dataset(image_dir, image_ext, label_dir, batch_size, name=""):
+    img_name_list = glob.glob(os.path.join(image_dir, "*" + image_ext))
+    lbl_name_list = []
+    for img_path in img_name_list:
+        img_name = img_path.split(os.sep)[-1]
+        aaa = img_name.split(".")
+        bbb = aaa[0:-1]
+        imidx = bbb[0]
+        for i in range(1, len(bbb)):
+            imidx = imidx + "." + bbb[i]
+        lbl_name_list.append(os.path.join(label_dir, imidx + "_s" + label_ext))
+
+    print("---")
+    print(f"{name} images: {len(img_name_list)}")
+    print(f"{name} labels: {len(lbl_name_list)}")
+    print("---")
+
+    salobj_dataset = SalObjDataset(
+        img_name_list=img_name_list,
+        lbl_name_list=lbl_name_list,
+        transform=transforms.Compose(
+            [RescaleT(320), RandomCrop(288), ToTensorLab(flag=0)]
+        ),
+    )
+    salobj_dataloader = DataLoader(
+        salobj_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=1,
+        drop_last=True,
+    )
+    return salobj_dataloader, len(img_name_list)
 
 
 def muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, labels_v):
 
+    bce_loss = nn.BCELoss(size_average=True)
     loss0 = bce_loss(d0, labels_v)
     loss1 = bce_loss(d1, labels_v)
     loss2 = bce_loss(d2, labels_v)
@@ -38,79 +99,46 @@ def muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, labels_v):
     loss6 = bce_loss(d6, labels_v)
 
     loss = loss0 + loss1 + loss2 + loss3 + loss4 + loss5 + loss6
-    print(
-        "l0: %3f, l1: %3f, l2: %3f, l3: %3f, l4: %3f, l5: %3f, l6: %3f\n"
-        % (
-            loss0.item(),
-            loss1.item(),
-            loss2.item(),
-            loss3.item(),
-            loss4.item(),
-            loss5.item(),
-            loss6.item(),
-        )
-    )
-
     return loss0, loss
 
 
 # ------- 2. set the directory of training dataset --------
 
-model_name = "u2net"  #'u2netp'
-
-data_dir = "/root/TrainingDataset/TrainingData/"
-tra_image_dir = "Images/"
-tra_label_dir = "TD_FixMaps/"
+data_dir = "/root/TrainingDataset/TrainingData"
+tra_image_dir = os.path.join(data_dir, "ImagesTrain")
+tra_label_dir = os.path.join(data_dir, "TD_FixMaps")
+val_image_dir = os.path.join(data_dir, "ImagesVal")
+val_label_dir = tra_label_dir
 
 image_ext = ".png"
 label_ext = ".png"
 
-model_dir = os.path.join(os.getcwd(), "saved_models", "cs386", model_name + os.sep)
+training_task_name = "pretrained"
+model_history_dir = "./saved_models/history/"
+best_model = "./saved_models/u2net-trained.pth"
+pretrained_model_path = "./saved_models/u2net.pth"
 
 epoch_num = 100000
 batch_size_train = 8
-batch_size_val = 1
-train_num = 0
-val_num = 0
+batch_size_val = 15
 
-tra_img_name_list = glob.glob(data_dir + tra_image_dir + "*" + image_ext)
-
-tra_lbl_name_list = []
-for img_path in tra_img_name_list:
-    img_name = img_path.split(os.sep)[-1]
-
-    aaa = img_name.split(".")
-    bbb = aaa[0:-1]
-    imidx = bbb[0]
-    for i in range(1, len(bbb)):
-        imidx = imidx + "." + bbb[i]
-
-    tra_lbl_name_list.append(data_dir + tra_label_dir + imidx + "_s" + label_ext)
-
-print("---")
-print("train images: ", len(tra_img_name_list))
-print("train labels: ", len(tra_lbl_name_list))
-print("---")
-
-train_num = len(tra_img_name_list)
-
-salobj_dataset = SalObjDataset(
-    img_name_list=tra_img_name_list,
-    lbl_name_list=tra_lbl_name_list,
-    transform=transforms.Compose([RescaleT(320), RandomCrop(288), ToTensorLab(flag=0)]),
+train_dataloader, train_num = load_dataset(
+    tra_image_dir, image_ext, tra_label_dir, batch_size_train, "train"
 )
-salobj_dataloader = DataLoader(
-    salobj_dataset, batch_size=batch_size_train, shuffle=True, num_workers=1
+val_dataloader, val_num = load_dataset(
+    val_image_dir, image_ext, val_label_dir, batch_size_val, "val"
 )
 
 # ------- 3. define model --------
 # define the net
-if model_name == "u2net":
-    net = U2NET(3, 1)
-elif model_name == "u2netp":
-    net = U2NETP(3, 1)
+net = U2NET(3, 1)
+if pretrained_model_path:
+    print(f"Try to load pretrained model from {pretrained_model_path}")
+    net.load_state_dict(torch.load(pretrained_model_path))
+    print("Success")
 
 if torch.cuda.is_available():
+    print("Use CUDA")
     net.cuda()
 
 # ------- 4. define optimizer --------
@@ -125,12 +153,19 @@ ite_num = 0
 running_loss = 0.0
 running_tar_loss = 0.0
 ite_num4val = 0
-save_frq = 2000  # save the model every 2000 iterations
+save_frq = 1000  # save the model every 1000 iterations
+
+min_loss = float("inf")
 
 for epoch in range(0, epoch_num):
     net.train()
-
-    for i, data in enumerate(salobj_dataloader):
+    val_loss, _ = validate(net, val_dataloader)
+    if val_loss < min_loss:
+        print("--- Save best model ---")
+        print(f"min loss: {min_loss:.3f} -> {val_loss:.3f}")
+        min_loss = val_loss
+        torch.save(net.state_dict(), best_model)
+    for i, data in enumerate(train_dataloader):
         ite_num = ite_num + 1
         ite_num4val = ite_num4val + 1
 
@@ -180,11 +215,11 @@ for epoch in range(0, epoch_num):
         )
 
         if ite_num % save_frq == 0:
-
+            print("--- Save model checkpoint ---")
             torch.save(
                 net.state_dict(),
-                model_dir
-                + model_name
+                model_history_dir
+                + training_task_name
                 + "_bce_itr_%d_train_%3f_tar_%3f.pth"
                 % (ite_num, running_loss / ite_num4val, running_tar_loss / ite_num4val),
             )
